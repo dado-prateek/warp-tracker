1c6950f6e198ed8306939aca0c8d7cd5ef6034db
qborg <q.borg@yandex.ru>
2016-03-04 16:29:48 +0300

    fix config

--- warp/config.py
+++ warp/config.py
@@ -4,9 +4,9 @@ import os
 
 cfg = {
     'port': 1717,
-    'bind_addr': '0.0.0.0',
+    'bind_addr': '127.0.0.1',
     'torrents_dir': os.path.join(os.getcwd(), 'torrents'),
-    'announce_url': 'http://example.com/announce ',
+    'announce_url': 'http://127.0.0.1:1717/announce',
 
     # Replace announce url in torrent file to given when send to client
     'patch_announce_url': True,
